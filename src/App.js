df801d5ad17407e0a3b8f648181f8bc5f2a29685
Besufekad Alemu <besurye@gmail.com>
2023-03-22 23:18:50 +0300

    remove unnecessary comment lines and refactor my component to a functional component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-// import logo from './logo.svg';
 import React from 'react';
 import Calc from './component/calc';
 import './App.css';
 
-/* eslint-disable react/prefer-stateless-function */
-class App extends React.Component {
-  render() {
-    return (<Calc />);
-  }
+function App() {
+  return (
+    <div>
+      <Calc />
+    </div>
+  );
 }
 
 export default App;
