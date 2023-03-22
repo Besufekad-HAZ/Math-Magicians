ad1ade3aecb373fddeeaa9455976998d1d510e74
Besufekad Alemu <besurye@gmail.com>
2023-03-22 22:29:45 +0300

    Create components UI for the calculator app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
+import Calc from './component/calc';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Setup Math Magicians Project using Create React App.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* eslint-disable react/prefer-stateless-function */
+class App extends React.Component {
+  render() {
+    return (<Calc />);
+  }
 }
 
 export default App;
